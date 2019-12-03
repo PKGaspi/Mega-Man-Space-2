23db018662313f2ec4d3ea56dadf0df43fd60948
Juan Gaspar García <gaxdpar@gmail.com>
2019-12-03 16:10:51 +0100

    Changed star generation logic to be static instead of dynamic.

--- godot_proyect/scripts/star_generator.gd
+++ godot_proyect/scripts/star_generator.gd
@@ -1,45 +1,49 @@
 extends ParallaxBackground
 
-const NUMBER_OF_LAYERS = 7
+#const STAR = preload("res://scenes/star.tscn")
+
+const NUMBER_OF_LAYERS = 100
+const MIN_MOTION_SCALE = .4
+const MAX_MOTION_SCALE = 1
 
 # How many stars are generated.
 # Must be between 0.0 and 1.0
-const STAR_FREQUENCY = .5 
+const STAR_FREQUENCY = .3 
 
-const WIDTH = 480 # Screen width.
-const HEIGHT = 270 # Screen height.
-const TILES_X = 10 # Max stars in a row.
-const TILES_Y = 10 # Max stars in a column.
-const TILE_OFFSET = 5 # Offset for columns and rows.
+const WIDTH = 1920 # Room width.
+const HEIGHT = 1080 # Room height.
+const TILES_X = 20 # Max stars in a row.
+const TILES_Y = 20 # Max stars in a column.
+const TILE_OFFSET = 10 # Offset for columns and rows.
 
 var random
 var r_seed
 var layers = []
 
 func _ready():
+	
+	# Create random generator.
 	random = RandomNumberGenerator.new()
-	r_seed = random.seed
+	random.seed *= OS.get_ticks_usec()
+	
+	# Create parallax layers.
 	for i in range(NUMBER_OF_LAYERS):
 		var layer = ParallaxLayer.new()
-		var scale = float(i + 1) / NUMBER_OF_LAYERS
+		# Interpolate motion scale.
+		var t = float(i + 1) / NUMBER_OF_LAYERS
+		var scale = MIN_MOTION_SCALE * (1 - t) + MAX_MOTION_SCALE * t
 		layer.motion_scale = Vector2(scale, scale)
 		layer.z_index = i
 		add_child(layer)
 		layers.append(layer)
-	
-func _process(delta):
-	for i in range(0, get_child_count()):
-		var layer = get_child(i)
-		for j in range(0, layer.get_child_count()):
-			layer.get_child(j).queue_free()
-	random = RandomNumberGenerator.new()
-	random.seed = r_seed
+		
+	# Create the stars.
 	for i in range(0, TILES_X):
 		for j in range(0, TILES_Y):
 			if (random.randf() < STAR_FREQUENCY):
-				var x = (float(i) / TILES_X) * WIDTH + random.randf() * TILES_X * TILE_OFFSET
+				var x = (float(i) / TILES_X) * WIDTH + (random.randf() - .5) * TILES_X * TILE_OFFSET
 				x -= WIDTH / 2
-				var y = (float(j) / TILES_Y) * HEIGHT + random.randf() * TILES_Y * TILE_OFFSET
+				var y = (float(j) / TILES_Y) * HEIGHT + (random.randf() - .5) * TILES_Y * TILE_OFFSET
 				y -= HEIGHT / 2
 				var layer = int(random.randf() * NUMBER_OF_LAYERS)
 				create_star(Vector2(x, y), layer, "res://assets/sprites/lemon.png")
