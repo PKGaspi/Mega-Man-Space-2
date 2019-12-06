9cc92373393ee6b9194a9a83d958175add5714c0
Gaspi <gaxdpar@gmail.com>
2019-12-06 20:22:31 +0100

    Implemented automatic star generation and destruction around the player.

--- godot_proyect/scripts/star_generator.gd
+++ godot_proyect/scripts/star_generator.gd
@@ -22,30 +22,40 @@ const PLANETS = [
 	preload("res://assets/sprites/planet_0.png"),
 ]
 
+# Initialize runtime constants.
+var STAR_MAX_SIZE = len(STAR_SIZES)
+var N_STAR_SPRITES = len(STARS)
+
 const N_LAYERS = 50
 const MIN_MOTION_SCALE = .4
 const MAX_MOTION_SCALE = 1
 
 # How many stars are generated.
 # Must be between 0.0 and 1.0
-const STAR_FREQUENCY = 1
+const STAR_FREQUENCY = .4
 
-const WIDTH = 30 # Room width.
-const HEIGHT = 30 # Room height.
-const TILES_X = 4 # Max stars in a row.
-const TILES_Y = 4 # Max stars in a column.
-const TILE_OFFSET = 0 # Offset for columns and rows.
+const SECTOR_WIDTH = 160 # Sector width.
+const SECTOR_HEIGHT = 100 # Sector height.
+const SECTOR_ROWS = 10 # Number of sectors loaded at the same time on a row.
+const SECTOR_COLUMNS = 10 # Number of sectors loaded at the same time on a column.
+const TILES_X = 4 # Max stars in a sector row.
+const TILES_Y = 4 # Max stars in a sector column.
+const TILE_OFFSET = 5 # Offset for columns and rows.
 
-var random
-var r_seed
-var layers = []
+var random # Base randomizer.
+var r_seed # Base random seed.
+var seeds = {} # Seeds for each sector.
+var stars = {} # Dictionary with lists of stars for each sector.
+var layers = [] # Array of layers.
 
-# Initialize runtime constants.
-var STAR_MAX_SIZE = len(STAR_SIZES)
-var N_STAR_SPRITES = len(STARS)
+var prev_sector # Megaship last sector.
+
+var MEGASHIP
 
 func _ready():
 	
+	MEGASHIP = get_tree().get_root().get_child(0).get_node("Megaship")
+	prev_sector = Vector2(1000, 1000)
 	# Create random generator.
 	random = RandomNumberGenerator.new()
 	random.seed *= OS.get_ticks_usec()
@@ -62,31 +72,64 @@ func _ready():
 		add_child(layer)
 		layers.append(layer)
 		
-	create_stars(Vector2(0, 0))
-	create_stars(Vector2(1, 0))
-	create_stars(Vector2(0, 1))
-	create_stars(Vector2(1, 1))
-	create_stars(Vector2(-1, 0))
-	create_stars(Vector2(0, -1))
-	create_stars(Vector2(-1, -1))
-	create_stars(Vector2(1, -1))
-	create_stars(Vector2(-1, 1))
-	create_stars(Vector2(-5, 3))
-		
+	create_stars(prev_sector)
+	
+func _process(delta):
+	var sector = pos_to_sector(MEGASHIP.position)
+	var sector_x = sector.x
+	var sector_y = sector.y
+	if prev_sector != sector:
+		for s in stars.keys():
+			if sector.distance_to(s) >= 2:
+				destroy_stars(s)
+		for i in range(-SECTOR_ROWS / 2, SECTOR_ROWS / 2):
+			for j in range(-SECTOR_COLUMNS / 2, SECTOR_COLUMNS / 2):
+				var new_sector = Vector2(sector_x + i, sector_y + j)
+				if !stars.has(new_sector):
+					create_stars(new_sector)
+		prev_sector = sector
+
+
+func pos_to_sector(pos):
+	# Vector2 pos: position to check if is on the sector.
+	var sector_x = int(pos.x / SECTOR_WIDTH)
+	var sector_y = int(pos.y / SECTOR_HEIGHT)
+	return Vector2(sector_x, sector_y)
+
+func create_star(pos, layer, sprite):
+	# Vector2 pos: position of the star.
+	# int layer: layer index to place the star.
+	# String sprite: route of the texture of the star.
+	var star = Sprite.new()
+	star.z_index = layers[layer].z_index
+	star.position = pos * layers[layer].motion_scale
+	star.texture = sprite
+	layers[layer].add_child(star)
+	return star
+
 func create_stars(sector):
 	# Calculate sector coordinates.
-	var center_x = (WIDTH * sector.x)
-	var center_y = (HEIGHT * sector.y)
+	var center_x = (SECTOR_WIDTH * sector.x)
+	var center_y = (SECTOR_HEIGHT * sector.y)
+	
+	# Create list entry in the dictionary of stars.
+	stars[sector] = []
+	
+	# Set the seed.
+	if !seeds.has(sector):
+		seeds[sector] = r_seed * OS.get_ticks_usec()
+	random.seed = seeds[sector]
+	
 	# Create the stars.
 	var n_planetas = 0
 	for i in range(0, TILES_X):
 		for j in range(0, TILES_Y):
 			if (random.randf() < STAR_FREQUENCY):
 				# Generate random position.
-				var x = (float(i) / TILES_X) * WIDTH + (random.randf() - .5) * TILES_X * TILE_OFFSET
-				x += center_x - WIDTH / 2
-				var y = (float(j) / TILES_Y) * HEIGHT + (random.randf() - .5) * TILES_Y * TILE_OFFSET
-				y += center_y - HEIGHT / 2
+				var x = (float(i) / TILES_X) * SECTOR_WIDTH + (random.randf() - .5) * TILES_X * TILE_OFFSET
+				x += center_x
+				var y = (float(j) / TILES_Y) * SECTOR_HEIGHT + (random.randf() - .5) * TILES_Y * TILE_OFFSET
+				y += center_y
 				
 				# Generate random layer.
 				var layer = random.randi_range(0, N_LAYERS - 1)
@@ -108,14 +151,10 @@ func create_stars(sector):
 					#sprite = PLANETS[random.randi_range(0, len(PLANETS) - 1)]
 					n_planetas += 1
 				# Create the star.
-				create_star(Vector2(x, y), 1, sprite)
+				stars[sector].append(create_star(Vector2(x, y), layer, sprite))
 
-func create_star(pos, layer, sprite):
-	# Vector2 pos: position of the star.
-	# int layer: layer index to place the star.
-	# String sprite: route of the texture of the star.
-	var star = Sprite.new()
-	star.z_index = layers[layer].z_index
-	star.position = pos
-	star.texture = sprite
-	layers[layer].add_child(star)
+func destroy_stars(sector):
+	# Calculate sector coordinates.
+	for star in stars[sector]:
+		star.queue_free()
+	stars.erase(sector)
