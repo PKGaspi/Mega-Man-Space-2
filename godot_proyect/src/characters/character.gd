4128537fd1a4a1a0be43c34f5cab67f550876468
Juan Gaspar García <gaxdpar@gmail.com>
2020-02-02 14:37:07 +0100

    Started weapons menu

--- godot_proyect/src/characters/character.gd
+++ godot_proyect/src/characters/character.gd
@@ -1,52 +1,50 @@
 extends KinematicBody2D
 
-onready var BAR_CONTAINER = $"/root/Space/GUILayer/Container/BarContainer"
 
 # Bars.
 const PROGRESS_BAR = preload("res://src/gui/progress_bar.tscn")
+onready var BAR_CONTAINER = $"/root/Space/GUILayer/Container/BarContainer"
 # HP.
+export(float, 0, 100, 1) var hp_max = 10 # Max hp.
 export(bool) var _hp_bar_show = true
 export(bool) var _hp_bar_on_gui = false
 export(int) var _hp_bar_palette = 0
 export(Vector2) var _hp_bar_cell_size = Vector2(4, 2)
 export(Vector2) var _hp_bar_position = Vector2(10, -8)
 var hp_bar
+var hp : float # Hp.
 # Ammo.
+export(float, 0, 100, 1) var ammo_max = 28 # Max ammo.
 export(bool) var _ammo_bar_show : bool = false
 export(bool) var _ammo_bar_on_gui : bool = false
 export(int) var _ammo_bar_palette : int = 0
 export(Vector2) var _ammo_bar_cell_size : Vector2 = Vector2(4, 2)
 export(Vector2) var _ammo_bar_position : Vector2 = Vector2(15, -8)
 var ammo_bar
-
-export(float, 0, 100, 1) var hp_max = 10 # Max hp.
-export(float, 0, 100, 1) var ammo_max = 28 # Max ammo.
+var ammo : float # Ammo.
 
 export(NodePath) var snd_hit = "SndHit"
 export(NodePath) var snd_shoot = "SndShoot"
 export(PackedScene) var death_instance = null
 
-var hp : float # Hp.
-var ammo : float # Ammo.
-
+# Flickering and invencibility.
+export(float) var invencibility_time : float = .5 # Seconds the character is invencible after hit.
+export(float) var life_time : float = 10 # Seconds until the character disapears.
+export(float) var life_flicker_time : float = 8 # Start flickering when the character has this many seconds of life.
+export(bool) var flicker_on_hit : bool = true
+export(bool) var flicker_before_timeout : bool = false
+export(bool) var dissapear_on_timeout : bool = false
+var flickering_interval : float = .05 # Seconds between each visibility toggle when flickering.
+var flickering_timer : float = 0 # Seconds until a toggle on visibility is made.
 var invencible : bool = false
 var flickering : bool = false
 var disappearing : bool = false
 
-export(float) var invencibility_time = .5 # Seconds the character is invencible after hit.
-export(float) var life_time = 10 # Seconds until the character disapears.
-export(float) var life_flicker_time = 8 # Start flickering when the character has this many seconds of life.
-export(bool) var flicker_on_hit = true
-export(bool) var flicker_before_timeout = false
-export(bool) var dissapear_on_timeout = false
-var flickering_interval = .05 # Seconds between each visibility toggle when flickering.
-var flickering_timer = 0 # Seconds until a toggle on visibility is made.
-
 # Bullets.
 export(PackedScene) var bullet = null
 export(Array, Array, Vector2) var cannon_pos = [[Vector2()]]
-export(int) var bullet_max = 3
-export(int) var n_cannons = 1
+export(int) var bullet_max : int = 3
+export(int) var n_cannons : int = 1
 
 var WEAPONS = global.WEAPONS # WEAPONS enum.
 
@@ -63,9 +61,9 @@ export(Dictionary) var DAMAGE_MULTIPLIERS = {
 }
 
 # Motion.
-var acceleration = 1
-var momentum : Vector2 = Vector2()
-var friction = .2
+var acceleration : float = 1
+var momentum : Vector2 = Vector2.ZERO
+var friction : float = .2
 
 # Signals.
 signal death
