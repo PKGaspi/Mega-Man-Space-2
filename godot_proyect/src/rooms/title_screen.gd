12300b00a6d93f7e35be635ac6dc0e4c385a6bbb
Gaspi <gaxdpar@gmail.com>
2020-02-04 21:16:48 +0100

    Added music to title screen and tweaked animation

--- godot_proyect/src/rooms/title_screen.gd
+++ godot_proyect/src/rooms/title_screen.gd
@@ -1,18 +1,21 @@
 extends Control
 
-var to_move : Vector2 = Vector2(0, 10)
+var to_move : Vector2 = Vector2(0, 100)
 func _ready() -> void:
+	$Music.play()
 	animate_camera()
-
+	
 func _on_Tween_tween_completed(object: Object, key: NodePath) -> void:
 	animate_camera()
 	
 func animate_camera() -> void:
+	to_move = to_move.rotated(randf() * PI)
 	var tween = $Tween
 	var camera = $Camera
 	tween.interpolate_property(camera, "position",
 		camera.position, camera.position + to_move,
-		3, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+		5, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
 	tween.start()
 	
-	to_move = -to_move
+
+
