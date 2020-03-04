fa50c516a2e565beef7f145fa5acf84a07d85052
Juan Gaspar García <gaxdpar@gmail.com>
2020-03-04 15:23:17 +0100

    Created base class MenuPanel and implemented Weapons menu using it

---
+++ godot_proyect/src/gui/menus/menu_panel.gd
@@ -0,0 +1,123 @@
+class_name MenuPanel
+extends Control
+
+const PALETTES : SpriteFrames = preload("res://resources/gui/menu_palettes.tres")
+
+var tween = Tween.new()
+var flickering_timer = Timer.new()
+export var _selected_flickering_interval: float = 8.0/60.0
+export var opening_time: = .3
+export var hide_when_animating: NodePath
+export var background: NodePath = "Background"
+
+var active:= true
+var unlocked_entries: Dictionary
+var entries:= []
+var entry: Node
+var entry_index: int = 1
+var n_entries: int = 0
+
+export var snd_selection_change: NodePath = "SndMenuSelect"
+
+signal opened
+signal closed
+signal animation_ended
+
+func _ready() -> void:
+	# Setup tween.
+	add_child(tween)
+	# Setup flickering timer.
+	add_child(flickering_timer)
+	flickering_timer.wait_time = _selected_flickering_interval
+	flickering_timer.connect("timeout", self, "_on_FlickeringTimer_timeout")
+	flickering_timer.start()
+	
+func _input(event: InputEvent) -> void:
+	if active:
+		if event.is_action_pressed("ui_down"):
+			accept_event()
+			_on_action_pressed_ui_down()
+		elif event.is_action_pressed("ui_up"):
+			accept_event()
+			_on_action_pressed_ui_up()
+		elif event.is_action_pressed("ui_left"):
+			accept_event()
+			_on_action_pressed_ui_left()
+		elif event.is_action_pressed("ui_right"):
+			accept_event()
+			_on_action_pressed_ui_right()
+		elif event.is_action_pressed("ui_accept"):
+			accept_event()
+			_on_action_pressed_ui_accept()
+
+func _on_action_pressed_ui_down():
+	next_entry()
+	
+func _on_action_pressed_ui_up():
+	previous_entry()
+
+func _on_action_pressed_ui_left():
+	pass
+
+func _on_action_pressed_ui_right():
+	_on_action_pressed_ui_accept()
+	
+func _on_action_pressed_ui_accept():
+	pass
+
+func _on_FlickeringTimer_timeout() -> void:
+	entry.modulate.a = 0 if entry.modulate.a == 1 else 1
+
+func growing_animation(start_size: Vector2, final_size: Vector2, time: float = opening_time, hide:= get_node(hide_when_animating)):
+	if hide != null: hide.visible = false
+	# Start opening animation.
+	tween.interpolate_property(self, "rect_size", start_size, final_size, time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.interpolate_property(self, "rect_position", rect_position + final_size / 2, rect_position + start_size / 2, time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+	yield(tween, "tween_all_completed") # Wait until the animation is over.
+	# Animation is over.
+	if hide != null: hide.visible = true
+	emit_signal("animation_ended")
+
+func opening_animation(time = opening_time):
+	growing_animation(Vector2.ZERO, rect_size, time)
+	yield(self, "animation_ended")
+	emit_signal("opened")
+
+func closing_animation(time = opening_time):
+	growing_animation(rect_size, Vector2.ZERO, time)
+	yield(self, "animation_ended")
+	emit_signal("closed")
+
+func set_entry(value : int, play_sound: bool = true) -> bool:
+# warning-ignore:narrowing_conversion
+	value = clamp(value, 0, n_entries)
+	if value < entries.size():
+		if play_sound and snd_selection_change != null: get_node(snd_selection_change).play()
+		if entry != null:
+			entry.modulate.a = 1
+		entry_index = value
+		entry = entries[entry_index]
+		return true
+	return false
+
+func next_entry() -> void:
+	if n_entries != 0:
+		var new_entry = (entry_index + 1) % n_entries
+		while entries[new_entry] == null:
+			new_entry = (new_entry + 1) % n_entries
+		set_entry(new_entry)
+	
+func previous_entry() -> void:
+	if n_entries != 0:
+		var new_entry = (entry_index - 1) % n_entries if entry_index > 0 else n_entries - 1
+		while entries[new_entry] == null:
+			new_entry = (new_entry - 1) % n_entries if new_entry > 0 else n_entries - 1
+		set_entry(new_entry)
+
+func update_entries() -> void:
+	pass
+
+func set_palette(value : int) -> void:
+	if value < PALETTES.get_frame_count("default"):
+		get_node(background).material.set_shader_param("palette", PALETTES.get_frame("default", value))
--- godot_proyect/src/gui/menus/weapon menu/weapon_menu.gd
+++ godot_proyect/src/gui/menus/weapon menu/weapon_menu.gd
@@ -1,17 +1,6 @@
-extends Control
+extends MenuPanel
 
-const PALETTES : SpriteFrames = preload("res://resources/gui/menu_palettes.tres")
-
-const OPENNING_TIME = .3
-
-var unlocked_entries : Dictionary
-var entries = []
-var entry : Node
-var entry_index : int = 1
-var n_entries : int = 0
-
-signal opened
-signal closed
+onready var pager = get_node("MarginContainer/Pager")
 
 func _ready() -> void:
 	global.connect("user_pause", self, "_on_global_user_pause")
@@ -19,42 +8,35 @@ func _ready() -> void:
 	
 	# Load menu entries.
 	update_entries()
-	$FlickeringTimer.start()
+	flickering_timer.start()
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_down"):
-		accept_event()
-		next_entry()
-	if event.is_action_pressed("ui_up"):
-		accept_event()
-		previous_entry()
-	if event.is_action_pressed("ui_left"):
-		accept_event()
-		next_page()
-	if event.is_action_pressed("ui_right"):
-		accept_event()
-		previous_page()
-	if event.is_action_pressed("ui_accept"):
-		accept_event()
-		match entry_index:
-			0:
-				# Next page.
-				next_page()
-			7:
-				# Use an e-tank.
-				if global.MEGASHIP != null and global.etanks > 0:
-					global.etanks -= 1
-					global.MEGASHIP.call_deferred("set_hp", global.MEGASHIP.hp_max, true)
-					global.set_user_pause(false)
-			8:
-				# TODO: Open settings menu.
-				pass
-			_:
-				# Set weapon.
-				if global.MEGASHIP != null and global.MEGASHIP.has_method("set_weapon"):
-					global.MEGASHIP.set_weapon($MarginContainer/Pager.page_index * 6 + entry_index - 1, false)
-					global.set_user_pause(false)
+func _on_action_pressed_ui_left():
+	previous_page()
 
+func _on_action_pressed_ui_right():
+	next_page()
+	
+
+func _on_action_pressed_ui_accept():
+	match entry_index:
+		0:
+			# Next page.
+			next_page()
+		7:
+			# Use an e-tank.
+			if global.MEGASHIP != null and global.etanks > 0:
+				global.etanks -= 1
+				global.MEGASHIP.call_deferred("set_hp", global.MEGASHIP.hp_max, true)
+				global.set_user_pause(false)
+		8:
+			# TODO: Open settings menu.
+			pass
+		_:
+			# Set weapon.
+			if global.MEGASHIP != null and global.MEGASHIP.has_method("set_weapon"):
+				global.MEGASHIP.set_weapon(pager.page_index * 6 + entry_index - 1, false)
+				global.set_user_pause(false)
+	
 func _on_global_user_pause(value : bool) -> void:
 	if !value:
 		closing_animation()
@@ -64,78 +46,36 @@ func _on_global_user_pause(value : bool) -> void:
 func _on_FlickeringTimer_timeout() -> void:
 	entry.modulate.a = 0 if entry.modulate.a == 1 else 1
 
-func growing_animation(start_size: Vector2, final_size: Vector2, time: float = OPENNING_TIME):
-	# Start opening animation.
-	$Tween.interpolate_property(self, "rect_size", start_size, final_size, time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	$Tween.interpolate_property(self, "rect_position", rect_position + final_size / 2, rect_position + start_size / 2, time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	$Tween.start()
-	# Animation is over.
-
-func opening_animation(time = OPENNING_TIME):
-	$MarginContainer.visible = false
-	growing_animation(Vector2.ZERO, rect_size, time)
-	yield($Tween, "tween_all_completed") # Wait until the animation is over.
-	$MarginContainer.visible = true
-	emit_signal("opened")
-
-func closing_animation(time = OPENNING_TIME):
-	$MarginContainer.visible = false
-	growing_animation(rect_size, Vector2.ZERO, time)
-	yield($Tween, "tween_all_completed") # Wait until the animation is over.
-	$MarginContainer.visible = true
-	emit_signal("closed")
-
-
 func next_page() -> void:
 	entry.modulate.a = 1
-	$MarginContainer/Pager.next_page()
+	pager.next_page()
 	update_entries()
 
 func previous_page() -> void:
 	entry.modulate.a = 1
-	$MarginContainer/Pager.previous_page()
+	pager.previous_page()
 	update_entries()
-
-func set_entry(value : int, play_sound: bool = true) -> void:
-# warning-ignore:narrowing_conversion
-	value = clamp(value, 0, n_entries)
-	if value < entries.size():
-		if play_sound: $SndMenuSelect.play()
-		if entry != null:
-			entry.modulate.a = 1
-		entry_index = value
-		entry = entries[entry_index]
-
-func next_entry() -> void:
-	if n_entries != 0:
-		var new_entry = (entry_index + 1) % n_entries
-		while entries[new_entry] == null:
-			new_entry = (new_entry + 1) % n_entries
-		set_entry(new_entry)
-	
-func previous_entry() -> void:
-	if n_entries != 0:
-		var new_entry = (entry_index - 1) % n_entries if entry_index > 0 else n_entries - 1
-		while entries[new_entry] == null:
-			new_entry = (new_entry - 1) % n_entries if new_entry > 0 else n_entries - 1
-		set_entry(new_entry)
 
 func update_entries() -> void:
 	entries = []
 	n_entries = 0
-	var page = $MarginContainer/Pager.page_index
-	for entry in $MarginContainer/Pager.current_page.get_node("Letters").get_children():
+	var page = pager.page_index
+	
+	for entry in pager.current_page.get_node("Letters").get_children():
 		var key = Vector2(page, n_entries)
 		var node = get_node("MarginContainer/Pager/Page" + str(page) + "/Info/" + entry.name)
+		
 		if key == Vector2(0, 7):
 			# e-tank entry.
 			entries.append(entry)
 			for i in range(global.etanks, 4):
 				node.get_node(str(i + 1)).visible = false
+				
 		elif key == Vector2(1, 7):
 			# 1up entry.
 			entries.append(null) # Make this entry non selectable.
 			node.get_node("Text").text = ":  %02d" % global.lifes
+			
 		elif !unlocked_entries.has(Vector2(key)) or unlocked_entries[key]:
 			# No info about current entry or entry is unlocked.
 			entries.append(entry)
@@ -151,7 +91,3 @@ func update_entries() -> void:
 				node.modulate.a = 0
 		n_entries += 1
 	entry = entries[entry_index]
-
-func set_palette(value : int) -> void:
-	if value < PALETTES.get_frame_count("default"):
-		$Background.material.set_shader_param("palette", PALETTES.get_frame("default", value))
