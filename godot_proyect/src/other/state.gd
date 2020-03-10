a61f1948f6f7f764549c61b1028f4b15aec726a7
Juan Gaspar García <gaxdpar@gmail.com>
2020-03-10 23:06:47 +0100

    Added state machines
    Code from https://github.com/GDquest/godot-3d-mannequin/

---
+++ godot_proyect/src/other/state.gd
@@ -0,0 +1,45 @@
+class_name State
+extends Node
+
+
+onready var _state_machine: = _get_state_machine()
+
+# Using the same class, i.e. State, as a type hint causes a memory leak in Godot
+# 3.2.
+var _parent_state: State = null # Null if the parent is the state machine.
+
+
+func _ready() -> void:
+	yield(owner, "ready")
+	var parent = get_parent()
+	if parent is State:
+		_parent_state = parent
+
+
+func unhandled_input(event: InputEvent) -> void:
+	if _parent_state != null:
+		_parent_state.unhandled_input(event)
+
+
+func process(delta: float) -> void:
+	if _parent_state != null:
+		_parent_state.process(delta)
+
+
+func physics_process(delta: float) -> void:
+	if _parent_state != null:
+		_parent_state.physics_process(delta)
+
+
+func enter(msg := {}) -> void:
+	pass
+
+
+func exit() -> void:
+	pass
+
+
+func _get_state_machine(node: Node = self) -> Node:
+	if node != null and not node.is_in_group("state_machine"):
+		return _get_state_machine(node.get_parent())
+	return node
---
+++ godot_proyect/src/other/state_machine.gd
@@ -0,0 +1,46 @@
+class_name StateMachine
+extends Node
+
+export var initial_state:= NodePath()
+
+onready var state: State = get_node(initial_state) setget set_state
+
+signal transitioned(state_path)
+
+
+func _init() -> void:
+	add_to_group("state_machine")
+
+
+func _ready() -> void:
+	yield(owner, "ready")
+	state.enter()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	state.unhandled_input(event)
+
+
+func _process(delta: float) -> void:
+	state.process(delta)
+
+
+func _physics_process(delta: float) -> void:
+	state.physics_proces(delta)
+
+
+func transition_to(target_state_path: String, msg: Dictionary = {}) -> void:
+	if not has_node(target_state_path):
+		print_debug("Warning: State %s doesn't exist." % target_state_path)
+		return # Exit if there is no such state.
+	
+	var target_state = get_node(target_state_path)
+	state.exit()
+	set_state(target_state)
+	state.enter(msg)
+	emit_signal("transitioned", target_state)
+	
+
+
+func set_state(value: State) -> void:
+	state = value
