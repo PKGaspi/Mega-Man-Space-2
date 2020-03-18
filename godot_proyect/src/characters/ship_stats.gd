c4c8d1e96103fb29f7b01e30f942fe62175bfb92
Juan Gaspar García <gaxdpar@gmail.com>
2020-03-18 23:51:31 +0100

    Added ship_stats script

---
+++ godot_proyect/src/characters/ship_stats.gd
@@ -0,0 +1,9 @@
+class_name ShipStats
+extends Stats
+
+
+export var max_speed: float = 260
+export var max_hp: float = 28.0
+
+export var _cap_max_speed:= Vector2(200, 380)
+export var _cap_max_hp:= Vector2(20, 36)
