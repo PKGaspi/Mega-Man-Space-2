99509a123b52fb20d6ff7e9b0a8707f6a3d7c04c
Juan Gaspar García <gaxdpar@gmail.com>
2020-04-03 12:12:20 +0200

    Added EnemyWave class

---
+++ godot_proyect/src/library/enemy_wave.gd
@@ -0,0 +1,54 @@
+class_name EnemyWave
+extends Node2D
+
+
+export var enemies: Resource = WeightRandomizer.new()
+export var n_total_enemies: int
+var n_enemies: int = 0 # Current number of enemies spawned.
+export var n_max_enemies_at_once: int
+export var radious: float = 500
+
+onready var rng := global.init_random()
+
+signal completed()
+
+func _ready() -> void:
+	assert(enemies is WeightRandomizer)
+	enemies.initialize()
+
+
+func _physics_process(delta: float) -> void:
+	while n_enemies < n_max_enemies_at_once and n_total_enemies > 0:
+		random_spawn()
+
+
+func random_spawn() -> void:
+	var enemy = enemies.get_random_item()
+	
+	# TODO: Avoid spawning enemies on-screen.
+	var x = rng.randf_range(-radious, radious)
+	var y = rng.randf_range(-radious, radious)
+	var pos = global_position + Vector2(x, y)
+	
+	enemy_spawn(enemy, pos)
+	
+
+
+func enemy_spawn(enemy: PackedScene, pos: Vector2) -> void:
+	var inst = enemy.instance()
+	inst.global_position = pos
+	inst.connect("tree_exited", self, "_on_enemy_tree_exited")
+	
+	n_total_enemies -= 1
+	n_enemies += 1
+	ObjectRegistry.register_node(inst)
+	
+
+
+func _on_enemy_tree_exited() -> void:
+	n_enemies -= 1
+	
+	# Check if the wave is defeated.
+	if n_total_enemies <= 0 and ObjectRegistry._enemies.get_child_count() == 0:
+		emit_signal("completed")
+		queue_free()
