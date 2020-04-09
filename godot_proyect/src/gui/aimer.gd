3d954408d55ddc77291a779f55c11c458c16ed2e
Juan Gaspar García <gaxdpar@gmail.com>
2020-04-09 13:31:07 +0200

    Remade aimer
    Now it also works with gamepad

--- godot_proyect/src/gui/aimer.gd
+++ godot_proyect/src/gui/aimer.gd
@@ -1,6 +1,8 @@
-extends TextureRect
+class_name Aimer
+extends Sprite
 
 var size
+var radious := 100.0
 
 func _ready() -> void:
 	size = texture.get_size()
@@ -9,8 +11,22 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	match global.input_type:
 		global.INPUT_TYPES.KEY_MOUSE:
+			global_position = get_global_mouse_position()
 			visible = true
-			rect_position = get_viewport().get_mouse_position() - size / 2
 		_:
-			visible = false
+			var dir = get_aiming_direction()
+			position = dir.rotated(-global_rotation) * radious
+			visible = dir.length() >= .1
 			# TODO: Set position around the MEGASHIP. Maybe a pointing sprite like warnings?
+
+
+func get_aiming_direction() -> Vector2:
+	var dir := Vector2(
+				Input.get_action_strength("aim_right") - Input.get_action_strength("aim_left"),
+				Input.get_action_strength("aim_down") - Input.get_action_strength("aim_up")
+			)
+	if dir.length() > 1:
+		dir = dir.normalized()
+	
+	print(dir)
+	return dir
