731fe41b2eb1ba8d62e07867a9331144f7a627f0
Juan Gaspar García <gaxdpar@gmail.com>
2020-04-26 21:54:17 +0200

    Added poll url and final tweacks to temp weapon get screen
    Closes #34

--- godot_proyect/src/rooms/weapon get/feedback_button.gd
+++ godot_proyect/src/rooms/weapon get/feedback_button.gd
@@ -1,8 +1,9 @@
 extends LinkButton
 
 
-const FEEDBACK_URL = "http://godotengine.org"
+const FEEDBACK_URL = "https://docs.google.com/forms/d/e/1FAIpQLScQ2tUlVv-GYwLXbqw1VAcreJpN3U2nWt-7w1DzZx-SbRFyVg/viewform?usp=sf_link"
 
 
 func _pressed() -> void:
 	OS.shell_open(FEEDBACK_URL)
+	global.exit_game()
