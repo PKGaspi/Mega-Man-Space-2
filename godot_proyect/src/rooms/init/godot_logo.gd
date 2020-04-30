c0e966e38719a9bea55db3b2bbb1415095f6f289
Juan Gaspar García <gaxdpar@gmail.com>
2020-04-30 13:05:58 +0200

    Implemented godot logo fade-in to init screen

--- godot_proyect/src/rooms/init/godot_logo.gd
+++ godot_proyect/src/rooms/init/godot_logo.gd
@@ -1,6 +1,5 @@
 extends VBoxContainer
 
-
 onready var tween := $Tween
 
 func _ready() -> void:
@@ -9,5 +8,5 @@ func _ready() -> void:
 
 func animate() -> void:
 	tween.interpolate_property(self, "modulate", Color(1, 1, 1, 0), Color(1, 1, 1, 1), 1, Tween.TRANS_SINE, Tween.EASE_IN, .3)
-	tween.interpolate_property(self, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 1, Tween.TRANS_SINE, Tween.EASE_IN, 6.3)
+	tween.interpolate_property(self, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 1, Tween.TRANS_SINE, Tween.EASE_IN, 2.8)
 	tween.start()
--- godot_proyect/src/rooms/init/init_screen.gd
+++ godot_proyect/src/rooms/init/init_screen.gd
@@ -2,18 +2,29 @@ extends Control
 
 const TITLE_SCREEN := "res://src/rooms/title screen/title_screen.tscn"
 
-onready var disclaimer := $Disclaimer
+onready var godot_logo := $CenterContainer/GodotLogo
+onready var disclaimer := $CenterContainer/Disclaimer
+
+var can_skip: bool = OS.is_debug_build()
 
 func _ready() -> void:
 	get_tree().current_scene = self
 
 
-func _on_LogoTween_tween_all_completed() -> void:
-	# Animate disclaimer.
-	disclaimer.animate()
+func _on_GaspiLogoTween_tween_all_completed() -> void:
+	# Animate GodotLogo.
+	godot_logo.animate()
 
+
+func _on_GodotLogoTween_tween_all_completed() -> void:
+	# Animate Disclaimer.
+	disclaimer.animate()
+	# Allow to skip the disclaimer if the game has been started once before.
+	# can_skip = can_skip or Config.get_can_skip_intro()
+	
 
 func _on_DisclaimerTween_tween_all_completed() -> void:
 	# Load title screen.
 	yield(get_tree().create_timer(.3), "timeout")
 	get_tree().change_scene(TITLE_SCREEN)
+	# Config.set_can_skip_intro(true)
